17d32ea33dc5ade7f4a1c6420ab2de7e79cfae63
Md Hasnain <khanhasnainali21@gmail.com>
2025-09-19 22:26:40 +0530

    Sanitized MT5 example file, removed all sensitive files

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
-           /* ....................... This is only Demo Code.................. */
+/* ....................... Demo Code (Commented) .................. */
 
-
- /*
+/*
 import io.mtapi.mt5.MT5Client;
 import io.mtapi.mt5.MessageHandler;
 import io.mtapi.mt5.exception.DecoderException;
@@ -10,124 +9,92 @@ import io.mtapi.mt5.network.enums.*;
 import io.mtapi.mt5.network.messages.req.NewOrderReq;
 import io.mtapi.mt5.network.messages.res.TradeEvent;
 import java.io.IOException;
+
 public class Main {
 
- // Replace these with your credentials
+    // Replace these with your credentials
+    private static final String HOST = "YOUR_HOST";
+    private static final int PORT = 443;
+    private static final int USERNAME = YOUR_USERNAME;
+    private static final String PASSWORD = "YOUR_PASSWORD";
 
- private static final String HOST = "15.188.127.36";
- private static final int PORT = 443;
- private static final int USERNAME = 111139222;
- private static final String PASSWORD = "3a40jP7!";
+    public static void main(String[] args) {
 
- public static void main(String[] args) {
+        // First, create the client instance
+        MT5Client mt5Client = new MT5Client(HOST, PORT, new MessageHandler() {
+            @Override
+            public void onMessage(MetaTraderMessage message, MT5Client client) {
+                System.out.println("OnMsgs   "+ System.nanoTime() + " - " + message.toString());
+            }
 
-  // First, create the client instance
-  MT5Client mt5Client = new MT5Client(HOST, PORT, new MessageHandler() {
-   @Override
-   public void onMessage(MetaTraderMessage message, MT5Client client) {
-    System.out.println("OnMsgs   "+ System.nanoTime() + " - " + message.toString());
-   }
+            @Override
+            public void onReceiveFailure(DecoderException e) { e.printStackTrace(); }
+            @Override
+            public void onSendFailure(Exception e) { e.printStackTrace(); }
+            @Override
+            public void onDisconnected(IOException exception, MT5Client client) {
+                exception.printStackTrace();
+                System.out.println("DISCONNECTED");
+            }
 
-   @Override
-   public void onReceiveFailure(DecoderException e) {
-    e.printStackTrace();
-   }
+            @Override
+            public void onConnected(MT5Client client) {
+                System.out.println("Connected");
+                System.out.println(client.getConnectedServer());
+            }
+        });
 
-   @Override
-   public void onSendFailure(Exception e) {
-    e.printStackTrace();
-   }
+        try {
+            System.out.println("Connecting to MT5...");
+            mt5Client.connect(USERNAME, PASSWORD, true, true);
 
-   @Override
-   public void onDisconnected(IOException exception, MT5Client client) {
-    exception.printStackTrace();
-    System.out.println("DISCONNECTED");
-   }
+            // Open the trade (demo)
+            NewOrderReq newOrderReq = new NewOrderReq();
+            newOrderReq.userId = YOUR_USERNAME;
+            newOrderReq.requestId = 123456;
+            newOrderReq.placedType = PlacedType.Manually;
+            newOrderReq.symbol = "SYMBOL";
+            newOrderReq.fillPolicy = FillPolicy.ImmediateOrCancel;
+            newOrderReq.orderType = OrderType.Buy;
+            newOrderReq.volume = 100000000;
+            newOrderReq.tradeType = TradeType.MarketExecution;
 
-   @Override
-   public void onConnected(MT5Client client) {
-    System.out.println("Connected");
-    System.out.println(client.getConnectedServer());
-   }
-  });
+            long startOpenTime = System.nanoTime();
+            TradeEvent tradeEvent = (TradeEvent) mt5Client.sendRequest(newOrderReq);
+            long endOpenTime = System.nanoTime();
+            System.out.println("Trade Result: " + tradeEvent);
 
-  try {
-   System.out.println("Connecting to MT5...");
-   mt5Client.connect(USERNAME, PASSWORD, true, true);
+            double openDurationMs = (endOpenTime - startOpenTime) / 1_000_000.0;
+            System.out.println("PURE ORDER EXECUTION: " +
+                    String.format("%.3f", openDurationMs) + " ms");
 
-   Thread.sleep(2000);
+            // Close trade (demo)
+            NewOrderReq closeOrderReq = new NewOrderReq();
+            closeOrderReq.userId = YOUR_USERNAME;
+            closeOrderReq.requestId = 123457;
+            closeOrderReq.placedType = PlacedType.Manually;
+            closeOrderReq.symbol = "SYMBOL";
+            closeOrderReq.fillPolicy = FillPolicy.ImmediateOrCancel;
+            closeOrderReq.digits = 2;
+            closeOrderReq.orderType = OrderType.Sell;
+            closeOrderReq.volume = 100;
+            closeOrderReq.tradeType = TradeType.MarketExecution;
+            closeOrderReq.deviation = 1000;
+            closeOrderReq.expirationType = ExpirationDateType.GTC;
+            closeOrderReq.dealTicket = tradeEvent.tradeResult.ticketNumber;
+            closeOrderReq.comment = "CLOSING TRADE";
 
-   // Open the trade
-   NewOrderReq newOrderReq = new NewOrderReq();
-   newOrderReq.userId = USERNAME;
-   newOrderReq.requestId = 123456;
-   newOrderReq.placedType = PlacedType.Manually;
-   newOrderReq.symbol = "XAUUSD";
-   newOrderReq.fillPolicy = FillPolicy.ImmediateOrCancel;
-   //newOrderReq.digits = 2;
-   newOrderReq.orderType = OrderType.Buy;
-   newOrderReq.volume = 100000000;
-   newOrderReq.tradeType = TradeType.MarketExecution;
-   // newOrderReq.deviation = 1000;
-  // newOrderReq.expirationType = ExpirationDateType.GTC;
-   //newOrderReq.comment = "NEW MT5 SDK";
+            mt5Client.sendRequestAsync(closeOrderReq);
+            mt5Client.disconnect();
 
-   // Measure ONLY the order execution
-   long startOpenTime = System.nanoTime();
-   TradeEvent tradeEvent = (TradeEvent) mt5Client.sendRequest(newOrderReq);
-   System.out.println("Trade Result: " + tradeEvent);
-   long endOpenTime = System.nanoTime();
-
-   double openDurationMs = (endOpenTime - startOpenTime) / 1_000_000.0;
-   System.out.println("PURE ORDER EXECUTION: " +
-           String.format("%.3f", openDurationMs) + " ms");
-
-
-   Thread.sleep(1000);
-   // Measure ONLY the order execution
-    startOpenTime = System.nanoTime();
-    TradeEvent  tradeEvent2 = (TradeEvent) mt5Client.sendRequest(newOrderReq);
-   System.out.println("Trade Result: " + tradeEvent2);
-    endOpenTime = System.nanoTime();
-
-    openDurationMs = (endOpenTime - startOpenTime) / 1_000_000.0;
-   System.out.println("PURE ORDER EXECUTION: " +
-           String.format("%.3f", openDurationMs) + " ms");
-
-   Thread.sleep(5000);
-
-   // Close the trade
-   NewOrderReq closeOrderReq = new NewOrderReq();
-   closeOrderReq.userId = USERNAME;
-   closeOrderReq.requestId = 123457;
-   closeOrderReq.placedType = PlacedType.Manually;
-   closeOrderReq.symbol = "XAUUSD";
-   closeOrderReq.fillPolicy = FillPolicy.ImmediateOrCancel;
-   closeOrderReq.digits = 2;
-   closeOrderReq.orderType = OrderType.Sell;
-   closeOrderReq.volume = 100;
-   closeOrderReq.tradeType = TradeType.MarketExecution;
-   closeOrderReq.deviation = 1000;
-   closeOrderReq.expirationType = ExpirationDateType.GTC;
-   closeOrderReq.dealTicket = tradeEvent.tradeResult.ticketNumber;
-   closeOrderReq.comment = "CLOSING TRADE";
-
-   mt5Client.sendRequestAsync(closeOrderReq);
-
-   Thread.sleep(5000);
-
-   // Disconnect
-   mt5Client.disconnect();
-   System.out.println("Disconnected successfully");
-
-  } catch (Exception e) {
-   e.printStackTrace();
-  }
- }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
 */
 
-                                                      /* ..........this is my real code ..........*/
+/* .......... Real Code (Sanitized) .......... */
 
 import io.mtapi.mt5.MT5Client;
 import io.mtapi.mt5.MessageHandler;
@@ -137,36 +104,28 @@ import io.mtapi.mt5.network.enums.*;
 import io.mtapi.mt5.network.messages.req.NewOrderReq;
 import io.mtapi.mt5.network.messages.res.TradeEvent;
 
-
 import java.io.IOException;
 
 public class Main {
 
- private static final String HOST = "15.188.127.36";
+ private static final String HOST = "YOUR_HOST";
  private static final int PORT = 443;
- private static final int USERNAME = 111139222;
- private static final String PASSWORD = "3a40jP7!";
- private static final String SYMBOL = "XAUUSD"; // single point of reference
+ private static final int USERNAME = YOUR_USERNAME;
+ private static final String PASSWORD = "YOUR_PASSWORD";
+ private static final String SYMBOL = "SYMBOL";
 
  public static void main(String[] args) {
 
   MT5Client mt5Client = new MT5Client(HOST, PORT, new MessageHandler() {
    @Override
    public void onMessage(MetaTraderMessage message, MT5Client client) {
-
     System.out.println("OnMsgs   " + System.nanoTime() + " - " + message.toString());
    }
 
    @Override
-   public void onReceiveFailure(DecoderException e) {
-    e.printStackTrace();
-   }
-
+   public void onReceiveFailure(DecoderException e) { e.printStackTrace(); }
    @Override
-   public void onSendFailure(Exception e) {
-    e.printStackTrace();
-   }
-
+   public void onSendFailure(Exception e) { e.printStackTrace(); }
    @Override
    public void onDisconnected(IOException exception, MT5Client client) {
     exception.printStackTrace();
@@ -185,8 +144,8 @@ public class Main {
    mt5Client.connect(USERNAME, PASSWORD, true, true);
 
    Thread.sleep(2000);
-                                        /*................... Open the trade.............*/
 
+   /*................... Open the trade.............*/
    NewOrderReq newOrderReq = new NewOrderReq();
    newOrderReq.userId = USERNAME;
    newOrderReq.requestId = 123456;
@@ -201,8 +160,8 @@ public class Main {
 
    long startOpenTime = System.nanoTime();
    TradeEvent tradeEvent = (TradeEvent) mt5Client.sendRequest(newOrderReq);
-   System.out.println("Trade Result: " + tradeEvent);
    long endOpenTime = System.nanoTime();
+   System.out.println("Trade Result: " + tradeEvent);
 
    double openDurationMs = (endOpenTime - startOpenTime) / 1_000_000.0;
    System.out.println("PURE ORDER EXECUTION: " +
@@ -212,8 +171,8 @@ public class Main {
 
    startOpenTime = System.nanoTime();
    TradeEvent tradeEvent2 = (TradeEvent) mt5Client.sendRequest(newOrderReq);
-   System.out.println("Trade Result: " + tradeEvent2);
    endOpenTime = System.nanoTime();
+   System.out.println("Trade Result: " + tradeEvent2);
 
    openDurationMs = (endOpenTime - startOpenTime) / 1_000_000.0;
    System.out.println("PURE ORDER EXECUTION: " +
@@ -221,7 +180,7 @@ public class Main {
 
    Thread.sleep(5000);
 
-                                             /*................... Close the trade.............*/
+   /*................... Close the trade.............*/
    NewOrderReq closeOrderReq = new NewOrderReq();
    closeOrderReq.userId = USERNAME;
    closeOrderReq.requestId = 123457;
@@ -241,7 +200,6 @@ public class Main {
    mt5Client.sendRequestAsync(closeOrderReq);
 
    Thread.sleep(5000);
-
    mt5Client.disconnect();
    System.out.println("Disconnected successfully");
 
